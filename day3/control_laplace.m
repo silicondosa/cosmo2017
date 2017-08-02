a7741e5acef8549523687b74bcf20b6a9342c231
Your Name <you@example.com>
2017-08-02 12:34:27 -0500

    Day 3 - Gunnar - saccade control - need to perform feedback using resettable integrator

---
+++ day3/control_laplace.m
@@ -0,0 +1,61 @@
+%% Time
+clear all;
+close;
+TEnd = 1000;
+t = 1:1:TEnd;
+T = [175 13];
+%% Construct impulse input
+delT = 1;
+impulseShift = 100;
+impulseIn = zeros(size(t));
+impulseIn(1+impulseShift :delT+impulseShift ) = 1/delT;
+
+
+%% Construct step input
+delT = 1;
+impulseShift = 100;
+impulseIn = zeros(size(t));
+impulseIn(1+impulseShift :end ) = 1;
+
+%% Instantiation
+x = zeros(size(t));
+dx = zeros(size(t));
+y = zeros(size(t));
+dy = zeros(size(t));
+
+%% Construct a resettable integrator
+
+
+%% Construct PI control
+in2 = zeros(size(t));
+I_in2 = zeros(size(t));
+for i = 2:(length(t))
+    I_in2(i) = I_in2(i-1) + I_in2(i);
+    in2(i)  = T(1) + I_in2(i);
+    dx(i+1) = (-x(i) + impulseIn(i))/T(1);
+end
+
+%% Construct stage 1
+for i = 1:(length(t)-1)
+    x(i+1)  = dx(i) + x(i);
+    dx(i+1) = (-x(i) + impulseIn(i))/T(1);
+end
+subplot(2,1,1);
+hold on
+plot(t,impulseIn/1000.0);
+plot(t,x)
+plot(t,dx)
+legend("Stage 1 input", "stage 1 position", "stage 1 velocity");
+%% Construct stage 2
+for i = 1:(length(t)-1)
+    y(i+1)  = dy(i) + y(i);
+    dy(i+1) = (-y(i) + x(i))/T(2);
+end
+subplot(2,1,2);
+hold on
+plot(t,impulseIn/1000.0);
+plot(t,x)
+plot(t,y)
+plot(t,dy)
+legend("Impulse Input", "Stage 2 input", "stage 2 position (impulse response)", "stage 2 velocity");
+
